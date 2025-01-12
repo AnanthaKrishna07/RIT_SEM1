51cb57aa3d0dfa344993283e8653575e815d212b
Ananthakrishna B <125133450+AnanthaKrishna07@users.noreply.github.com>
2025-01-12 10:32:04 +0530

    Create dash.php

---
+++ WEB_LAB/CYCLE 4 PHP/exp4/dash.php
@@ -0,0 +1,33 @@
+<?php 
+session_start(); // Start the session 
+ 
+include('db.php'); 
+ 
+if (!isset($_SESSION['user_id'])) { 
+    header("Location: login.php"); 
+    exit(); 
+} 
+ 
+$user_id = $_SESSION['user_id']; 
+$email = $_SESSION['email']; 
+ 
+$sql = "SELECT * FROM students WHERE id = '$user_id' AND email = '$email'"; 
+$result = $conn->query($sql); 
+ 
+if ($result->num_rows > 0) { 
+    $user = $result->fetch_assoc(); 
+} else { 
+    echo "User not found."; 
+    exit(); 
+} 
+?>
+<html> 
+<body> 
+    <h2>Welcome, <?php echo $user['first_name'] . ' ' . $user['last_name']; ?></h2> 
+    <p>Email: <?php echo $user['email']; ?></p> 
+    <p>Gender: <?php echo $user['gender']; ?></p> 
+    <p>Date of Birth: <?php echo $user['dob']; ?></p> 
+ 
+    <a href="logout.php">Logout</a> 
+</body> 
+</html> 
