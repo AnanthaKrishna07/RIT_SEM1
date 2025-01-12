2bdf2fa76b7f2f85381974e39e9b697e0f6af627
Ananthakrishna B <125133450+AnanthaKrishna07@users.noreply.github.com>
2025-01-12 19:46:30 +0530

    Create exp21.php

---
+++ WEB_LAB/weblabb_practice/exp21.php
@@ -0,0 +1,10 @@
+<?php
+$a=array('bibin', 'neymar', 'ashin','messi');
+print_r($a);
+echo("<br>ASCENDING ORDER:");
+asort($a);
+print_r($a);
+echo("<br>DECENDING ORDER:");
+arsort($a);
+print_r($a);
+?>
