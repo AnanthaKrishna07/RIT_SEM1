00d18f3c4bebb3874b5dea7cb0e65ada4580d5f7
Ananthakrishna B <125133450+AnanthaKrishna07@users.noreply.github.com>
2025-01-14 23:19:56 +0530

    Update exp7.py

--- PYTHON_LAB/CYCLE2/exp7.py
+++ PYTHON_LAB/CYCLE2/exp7.py
@@ -1,6 +1,7 @@
-color_list1=input("Enter color for list1 seperated by comma:" ).split(",")
-color_list2=input("Enter color for list2 seperated by comma:" ).split(",")
-color_list1=set(color_list1)
-color_list2=set(color_list2)
-result=color_list1-color_list2
-print("colors in list1 but not in list2 are: ",result)
+list1=input("Enter the color of list1").split(",")
+list2=input("Enter the color of list2").split(",")
+result=[]
+for char in list1:
+    if char not in list2:
+        result.append(char)
+print(f"color in list1 but not in list2 are: {result}")
